d4aeca0216cb00f2f28e9021536b9f6151e00c8a
Zackelynn Peralta <zperalta92@gmail.com>
2020-11-13 21:35:53 -0800

    testing commits

--- tests/app/test_howdy.py
+++ tests/app/test_howdy.py
@@ -1,6 +1,7 @@
-import app.howdy
+from app import howdy
 
 def test_return_msg() -> None:
     expected = "hello"
     result = howdy.return_msg('hello')
+    print(result)
     assert result == expected
